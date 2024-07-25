86b663cef806dc10cf5872b9663ae945631752ef
igor serkov <igor.serkov1005@gmail.com>
2024-07-25 16:59:34 +0300

    Fixed applyCustomDict func. Handled major bugs. ObjectMeta now creates class instead of instance

--- javascript/Object.py
+++ javascript/Object.py
@@ -1,33 +1,36 @@
 from __future__ import annotations as _
 
-from typing import Any
-from re import search, sub
-
+from typing import Any, Iterable, Optional
 
 __all__ = (
     'Object',
 )
 
 
-def applyCustomDict(dictString: str) -> dict:
-    while s := search(reExp := r'{+(?!.*{).*?}', dictString):
-        dictString = sub(reExp, f'CustomDict({list(eval(s.group()).items())})', dictString, count=1)
-    return eval(dictString)
+def applyCustomDict(data: Any) -> Any:
+    if isinstance(data, dict):
+        return CustomDict({key: applyCustomDict(value) for key, value in data.items()})
+    elif hasattr(data, '__iter__') and not isinstance(data, str):
+        return data.__class__(map(applyCustomDict, data))
+    return data
 
 
 class CustomDict(dict):
     def __getattr__(self, item) -> Any:
-        return super().get(item)
+        return self.get(item)
 
     def __setattr__(self, key, value) -> None:
-        return super().__setitem__(key, value)
+        return self.__setitem__(key, value)
+
+    def __setitem__(self, key, value) -> None:
+        return super().__setitem__(key, applyCustomDict(value))
 
     def __init__(self, seq=None, **kwargs) -> None:
         super().__init__()
         [self.__setitem__(key, value) for key, value in (dict(seq) | kwargs if seq else kwargs).items()]
 
     def __call__(self, seq=None, **kwargs) -> CustomDict:
-        [self.__setitem__(key, value) for key, value in applyCustomDict(str(dict(seq) | kwargs if seq else kwargs)).items()]
+        [self.__setitem__(key, value) for key, value in (dict(seq) | kwargs if seq else kwargs).items()]
         return self
 
     def __hash__(self) -> hash:
@@ -43,13 +46,28 @@ class CustomDict(dict):
             return self(other)
         return type(CustomDict)
 
+    def update(self, seq=None, **kwargs) -> None:
+        [self.__setitem__(key, value) for key, value in (dict(seq) | kwargs if seq else kwargs).items()]
+
+    @classmethod
+    def fromkeys(cls, iterable: Iterable, value: Optional[Any] = None) -> CustomDict:
+        return CustomDict({item: value for item in iterable})
+
+    def setdefault(self, key: str, default: Optional[Any] = None) -> Any:
+        if key not in self:
+            self.__setitem__(key, default)
+        return self.get(key)
+
 
 class ObjectMeta(type):
     def __new__(mcs, name, bases, attrs) -> CustomDict | Object:
         if not attrs.get('__check__'):
-            return CustomDict(attrs.get('__annotations__', {}))
+            return CustomDict
         return super().__new__(mcs, name, bases, attrs)
 
 
 class Object(metaclass=ObjectMeta):
     __check__ = True
+
+    def __init__(self, *args, **kwargs):
+        raise NotImplementedError('Object class should be used via inheriting')
